0fc125233ec66fe896581d2be7ca44f929bbb892
John Ceci <jceci@usa.m3.com>
2014-08-04 16:05:41 -0400

    gallery

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -11,8 +11,7 @@ angular.module('angularApp')
   .controller('GalleryCtrl', function ($scope) {
 
     $scope.gallery_list = [
-        {'name':'USVTA','gallery_id':'1'},{'name':'Monster Truck','gallery_id':'2'},{'name':'Oval','gallery_id':'3'}
+        {'name':'USVTA','gallery_id':'1','cover_image':'images/1.jpg'},{'name':'Monster Truck','gallery_id':'2','cover_image':'images/2.jpg'},{'name':'Oval','gallery_id':'3','cover_image':'images/3.jpg'}
     ];
-
 
   });
