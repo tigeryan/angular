b743b79046ce9836279f232f57800e1a8f6d9d4a
John Ceci <jceci@usa.m3.com>
2014-08-04 17:02:53 -0400

    gallery

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -8,25 +8,24 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-    .controller('GalleryCtrl', function ($scope) {
+    .controller('GalleryCtrl', function ($scope,$http) {
 
-        $scope.gallery_list = [
+        $scope.galleryList = [
             {'name':'USVTA','gallery_id':'1','cover_image':'images/1.jpg'},{'name':'Monster Truck','gallery_id':'2','cover_image':'images/2.jpg'},{'name':'Oval','gallery_id':'3','cover_image':'images/3.jpg'}
         ];
 
-
         $scope.myData = {};
-        $scope.myData.doClick = function(item, event) {
+        console.log('here');
+        var responsePromise = $http.get('http://localhost:8600/test.cfc?method=getlist2');
 
-            var responsePromise = $http.get("http://localhost:8600/test.cfc?method=getlist");
-
-            responsePromise.success(function(data, status, headers, config) {
-                $scope.myData.fromServer = data.DATA;
-            });
-            responsePromise.error(function(data, status, headers, config) {
-                alert("AJAX failed!");
-            });
-        }
+        responsePromise.success(function(data, status, headers, config) {
+            console.log('success: ' + data);
+            $scope.myData = data.DATA;
+            console.log(data.gallery_id[0]);
+        });
+        responsePromise.error(function(data, status, headers, config) {
+           console.log('AJAX failed!');
+        });
 
 
 
