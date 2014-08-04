e88545efdccdca13ede1b6d7ab9f8847144f77d8
John Ceci <jceci@usa.m3.com>
2014-08-04 17:11:54 -0400

    gallery

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -9,24 +9,23 @@
  */
 angular.module('angularApp')
     .controller('GalleryCtrl', function ($scope,$http) {
-
+        /*
         $scope.galleryList = [
             {'name':'USVTA','gallery_id':'1','cover_image':'images/1.jpg'},{'name':'Monster Truck','gallery_id':'2','cover_image':'images/2.jpg'},{'name':'Oval','gallery_id':'3','cover_image':'images/3.jpg'}
         ];
+        */
 
-        $scope.myData = {};
+        $scope.galleryList = {};
         console.log('here');
         var responsePromise = $http.get('http://localhost:8600/test.cfc?method=getlist2');
 
         responsePromise.success(function(data, status, headers, config) {
             console.log('success: ' + data);
-            $scope.myData = data.DATA;
-            console.log(data.gallery_id[0]);
+            $scope.galleryList = data;
+            console.log(data[0].gallery_id);
         });
         responsePromise.error(function(data, status, headers, config) {
            console.log('AJAX failed!');
         });
-
-
 
   });
