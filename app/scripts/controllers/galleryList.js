adb11855d896d5f73048b7f54007400ecbe8f5c5
John Ceci <tigeryan55@gmail.com>
2014-09-13 17:02:06 -0400

    next/prev

--- app/scripts/controllers/galleryList.js
+++ app/scripts/controllers/galleryList.js
@@ -8,12 +8,29 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
+
+	.filter('getByProperty', function() {
+		return function(propertyName, propertyValue, collection) {
+			var i=0, len=collection.length;
+			for (; i<len; i++) {
+				if (collection[i][propertyName] == +propertyValue) {
+					return collection[i];
+				}
+			}
+			return null;
+		}
+});
+
+
     .controller('GalleryListCtrl', function ($scope,$routeParams, $http) {
         /*
         $scope.galleryList = [
             {'name':'USVTA','gallery_id':'1','cover_image':'images/1.jpg'},{'name':'Monster Truck','gallery_id':'2','cover_image':'images/2.jpg'},{'name':'Oval','gallery_id':'3','cover_image':'images/3.jpg'}
         ];
         */
+
+
+
 
 		$scope.myData = {
 			fullImage: '',
@@ -28,6 +45,7 @@ angular.module('angularApp')
 
         responsePromise.success(function(data, status, headers, config) {
             $scope.imageList = data;
+			//console.log($scope.imageList.length);
 			//console.log($scope.imageList[0].party_title);
 			$scope.myData.partyTitle = $scope.imageList[0].party_title;
             //console.log($scope);
@@ -40,9 +58,23 @@ angular.module('angularApp')
 			//console.log('close!');
 		};
 
-		$scope.toggleModal = function(fullImage) {
+		$scope.nextModal = function(curImage) {
+			console.log(curImage);
+			$scope.myData.curImage = curImage;
+			console.log($scope.myData.curImage);
+
+
+
+			//$scope.myData.fullImage = $scope.imageList[myIndex].full_image;
+			//$scope.myData.modalShown = !$scope.myData.modalShown;
+
+		};
+
+		$scope.toggleModal = function(fullImage,myIndex) {
+			$scope.myData.myIndex = myIndex;
 			$scope.myData.fullImage = fullImage;
 			$scope.myData.modalShown = !$scope.myData.modalShown;
+			//console.log($scope.myData.myIndex);
 		};
 
 
