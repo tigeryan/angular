e18b69dcb3e5547ed941ab61a1aae2bf7d8fa3e2
John Ceci <jceci@usa.m3.com>
2014-10-15 08:43:06 -0400

    started homepage additions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,26 @@
  */
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
+
+    /*
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    */
+
+    $scope.galleryList = {};
+
+    var responsePromise = $http.get('http://beta.bodiesbybean.com/cfcs/bbb.cfc?method=getGalleryCovers');
+
+    responsePromise.success(function(data, status, headers, config) {
+        $scope.galleryList = data;
+    });
+
+    responsePromise.error(function(data, status, headers, config) {
+       console.log('AJAX failed!');
+    });
+
+
+});
