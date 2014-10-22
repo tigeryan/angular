134b9bb41507f229ca6479949dd16c52795082bc
John Ceci <jceci@usa.m3.com>
2014-10-22 16:50:08 -0400

    jslint

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,19 +9,28 @@
  */
 angular.module('angularApp')
 
-	.controller('MainCtrl', function ($scope,$http) {
+	.controller('MainCtrl', function ($scope, $http) {
+
+        // use this until working, then switch to galleryList
+        $scope.slides = [
+            {image: 'images/img00.jpg', description: 'Image 00'},
+            {image: 'images/img01.jpg', description: 'Image 01'},
+            {image: 'images/img02.jpg', description: 'Image 02'},
+            {image: 'images/img03.jpg', description: 'Image 03'},
+            {image: 'images/img04.jpg', description: 'Image 04'}
+        ];
 
         $scope.galleryList = {};
 
         var responsePromise = $http.get('http://beta.bodiesbybean.com/cfcs/bbb.cfc?method=getgallery');
 
-        responsePromise.success(function(data, status, headers, config) {
+        responsePromise.success(function (data, status, headers, config) {
             //console.log(data);
             $scope.galleryList = data;
            // console.log(data[0].gallery_id);
         });
-        responsePromise.error(function(data, status, headers, config) {
-           console.log('AJAX failed!');
+        responsePromise.error(function (data, status, headers, config) {
+            console.log('AJAX failed!');
         });
 
 	});
