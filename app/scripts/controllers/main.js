4dee0e751c48760d3599ed9771962713c624ec26
John Ceci <jceci@usa.m3.com>
2014-10-22 16:56:47 -0400

    slider

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,12 +13,31 @@ angular.module('angularApp')
 
         // use this until working, then switch to galleryList
         $scope.slides = [
-            {image: 'images/img00.jpg', description: 'Image 00'},
-            {image: 'images/img01.jpg', description: 'Image 01'},
-            {image: 'images/img02.jpg', description: 'Image 02'},
-            {image: 'images/img03.jpg', description: 'Image 03'},
-            {image: 'images/img04.jpg', description: 'Image 04'}
+            {image: 'http://www.bodiesbybean.com/images/gallery/953.jpg', description: 'Image 00'},
+            {image: 'http://www.bodiesbybean.com/images/gallery/136.jpg', description: 'Image 01'},
+            {image: 'http://www.bodiesbybean.com/images/gallery/953.jpg', description: 'Image 02'},
+            {image: 'http://www.bodiesbybean.com/images/gallery/136.jpg', description: 'Image 03'},
+            {image: 'http://www.bodiesbybean.com/images/gallery/953.jpg', description: 'Image 04'}
         ];
+
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+
 
         $scope.galleryList = {};
 
